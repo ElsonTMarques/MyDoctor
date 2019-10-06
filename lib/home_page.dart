f0f3fc93cd9b1f8102e83ce16bd29bfbd90af444
moises350 <moisescoelho122@gmail.com>
2019-10-06 20:14:46 -0300

    Novos componentes
    Ajustes no drawer, refatoração

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -48,35 +48,84 @@ class HomePage extends StatelessWidget {
     );
 
     return Scaffold(
-      appBar: AppBar(title: Text("My Doctor"), backgroundColor: Color(0xffABCFF2),),
+      appBar: AppBar(title: Text("Seja Bem-Vindo, Moisés"), backgroundColor: Color(0xffABCFF2),),
       body: body,
-      drawer: Drawer(
+      drawer: _createDrawerList(context),
+    );
+  }
+
+  Widget _createDrawerList(BuildContext context)
+  {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: (size.width) - 90,
+      child: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Menu'),
-              decoration: BoxDecoration(
-                color: Color(0xffABCFF2),
-              ),
-            ),
+            _createHeader(),
+            _createDrawerItem(icon: Icons.contacts,text: 'Receitas',),
+            _createDrawerItem(icon: Icons.contacts,text: 'Remédios',),
+            _createDrawerItem(icon: Icons.contacts,text: 'Médico',),
+            _createDrawerItem(icon: Icons.contacts,text: 'Sair',),
             ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
+              title: Text('0.0.1'),
+              onTap: () {},
             ),
           ],
         ),
       )
+    );
+  }
+
+  Widget _createHeader() 
+  {
+    return DrawerHeader(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      decoration: BoxDecoration(
+        color: Color(0xffABCFF2),
+      ),
+      child: Stack(children: <Widget>[
+        Positioned(
+            bottom: 12.0,
+            left: 100.0,
+            top: 70.0,
+            child: Text("Moisés Coelho",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w900
+                )
+            ),
+        ),
+        Positioned(
+            bottom: 50.0,
+            left: 16.0,
+            child: CircleAvatar(
+              maxRadius: 35,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('assets/default_person.jpg'),
+            ),
+        ),
+      ]
+      )
+    );
+  }
+
+  Widget _createDrawerItem({IconData icon, String text, GestureTapCallback onTap}) 
+  {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(text),
+          )
+        ],
+      ),
+      onTap: onTap,
     );
   }
 }
